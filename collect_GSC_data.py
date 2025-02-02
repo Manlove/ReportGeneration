2900a5517b00c755f592f9fa3bc7c5740d9a0003
Manlove <manlove.logan@gmail.com>
2025-02-01 23:36:30 -0600

    refractoring collect_GSC_data.py

--- collect_GSC_data.py
+++ collect_GSC_data.py
@@ -1,74 +1,79 @@
 #!/usr/bin/env python3
 
-# Run in a folder with subfolders for different weeks. These subfolders
-# contain weekly snapshots from the sharepoint analysis tracker and status summaries for each week.
-# Outputs a csv with the number of projects in each bucket for each week.
+"""
+Run in a folder with subfolders for different weeks. These subfolders
+contain weekly snapshots from the sharepoint analysis tracker and status summaries for each week.
+Outputs a csv with the number of projects in each bucket for each week.
+"""
 
 import glob
-from os import listdir
-import os.path
+import os
 
 def ClearCommas(line):
     """Replaces non-separating commas in a csv file with spaces"""
-    i=0
+    out_string = []
     quote_count = 0
-    while i < len(line):
+    
+    for char in line:
         
-        if line[i] == '"':
+        if char == '"':
             quote_count = 0 if quote_count == 1 else 1
             
-        elif line[i] == ',' and quote_count == 1:
-            line = line[:i] + " " + line[i+1:]
+        elif char == ',' and quote_count == 1:
+            out_string.append(" ")
+        else:
+            out_string.append(char)
             
-        i += 1
-    return line
+    return "".join(out_string)
 
-# Step through through each wekk and open the services anaysis tracker file.
-# Opens the files and retrieves the number of projects at each status. 
-with open("gsc_summary.csv", 'w') as summary_outfile:
-    gsc_summary = {"Week":[], "Analyzing":[], "Cancelled":[], "Not started":[],
-                   "On hold":[], "Sequencing incomplete":[], "Delivered":[]}
-    for week in listdir("."):
-        if os.path.isdir(os.path.join('.', week)):
-            if os.path.exists(os.path.join('.', week, "Services Analysis Tracker.csv")):
+
+if __name__ == "__main__":
+    # Step through through each week and open the services anaysis tracker file.
+    # Opens the files and retrieves the number of projects at each status. 
+    with open("gsc_summary.csv", 'w') as summary_outfile:
+        gsc_summary = {"Week":[], "Analyzing":[], "Cancelled":[], "Not started":[],
+                       "On hold":[], "Sequencing incomplete":[], "Delivered":[]
+                       }
+        for week in os.scandir("."):
+            tracker_path = os.path.join(week.path, "Services Analysis Tracker.csv")
+            status_summary_path = os.path.join(week.path, "status_summary.csv")
+
+            if not week.is_dir() or not os.path.exists(tracker_path):
+                continue
                 
-                with open(os.path.join('.', week, "Services Analysis Tracker.csv"), 'r') as apr_data:
-                    apr_data.readline()
-                    status_data = {}
-                    
-                    for line in apr_data.readlines():
-                        line = ClearCommas(line)
-                        line = line.replace('"','')
-                        line = line.strip().split(",")
+            with open(tracker_path, 'r') as apr_data:
+                next(apr_data)
+                status_data = {}
+                
+                for line in map(ClearCommas,apr_data):
+                    line = line.replace('"','').strip().split(",")
 
-                        # Data in the sharepoint output can be messy.
-                        # We drop lines with ten or less fields.
-                        if line[0].isnumeric() and len(line) > 10:
-                            status = line[6]
-                            if status in status_data:
-                                status_data[status] += 1
-                            else:
-                                status_data[status] = 1
-                                
-                gsc_summary["Week"].append(week)
+                    # Data in the sharepoint output can be messy.
+                    # We drop lines with ten or less fields.
+                    if line[0].isnumeric() and len(line) > 10:
+                        status = line[6]
+                        status_data[status] = status_data.get(status,0) + 1
 
-                # Transfers the data from the weekly summary to the final summary
-                for status in ["Analyzing", "Cancelled", "Not started",
-                               "On hold", "Sequencing incomplete"]:
-                        gsc_summary[status].append(status_data[status] if status in status_data else 0)
+                            
+            gsc_summary["Week"].append(week.name)
+
+            # Transfers the data from the weekly summary to the final summary
+            for status in ["Analyzing", "Cancelled", "Not started",
+                           "On hold", "Sequencing incomplete"]:
+                    gsc_summary[status].append(status_data.get(status, 0))
 
             # If no projects were delivered the status will not exist in status_summary.csv
             # The "delivered" status in ther services analysis tracker will have all projects
             # that have ever been delivered.
             delivered = 0
-            if os.path.exists(os.path.join('.', week, "status_summary.csv")):
-                with open(os.path.join('.', week, "status_summary.csv"), 'r') as apr_summary_data:
-                    apr_summary_data.readline()
-                    for line in apr_summary_data.readlines():
-                        line = line.replace('"','')
-                        line = line.strip().split(",")
+            if os.path.exists(status_summary_path):
+                with open(status_summary_path, 'r') as apr_summary_data:
+                    next(apr_summary_data)
+                    for line in apr_summary_data:
+                        line = line.replace('"','').strip().split(",")
                         if line[0] == "Delivered":
                             delivered = line[1]
+                            
             gsc_summary["Delivered"].append(delivered)
 
 
